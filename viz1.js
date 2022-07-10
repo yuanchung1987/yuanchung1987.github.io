e97fb47f71e98e003abe58dc875c30052840788d
Sean Ho <yuanchung1987@gmail.com>
2022-07-09 21:45:04 -0700

    update

--- viz1.js
+++ viz1.js
@@ -90,11 +90,5 @@ svg.selectAll("mybar")
     
 
 // Animation
-svg.selectAll("rect")
-  .transition()
-  .duration(800)
-  .attr("y", function(d) { return y(d.EstimatedHesitant); })
-  .attr("height", function(d) { return height - y(d.EstimatedHesitant); })
-  .delay(function(d,i){console.log(i) ; return(i*100)})
 
 })
